a04f064e365ef4153974c2e937d513e8f5ca138f
courd88 <teagyun@nate.com>
2024-01-17 17:30:23 +0900

    대충 화면만 나오게

--- src/MultiCare/Inquiry/Inquiry.js
+++ src/MultiCare/Inquiry/Inquiry.js
@@ -1,10 +1,27 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function Inquiry() {
+
+    const [ datas, setDatas ] = useState([])
+
+    useEffect(() => {
+        axios.get(`http://localhost:8080/api/inquiry`)
+            .then(response => {
+                console.log(response)
+                setDatas(response.data)
+            })
+            .catch(error => console.log(error))
+
+    }, [])
 
     return(
 
         <>
-        
+            <div>
+                <h2>멀티케어 문의</h2>
+                
+            </div>
         </>
     )
 }
--- src/MultiCare/Main.js
+++ src/MultiCare/Main.js
@@ -1,12 +1,31 @@
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 function Main() {
 
-    return(
+    const [ datas, setDatas ] = useState([])
+
+    useEffect(() => {
+        axios.get(`http://localhost:8080/api/inquiry`)
+            .then(response => {
+                console.log(response)
+                setDatas(response.data)
+            })
+            .catch(error => console.log(error))
+    }, [])
+
+    return (
 
         <>
-        
+            <div>
+            <h1>MultiCare Shop</h1>
+            <Link to="/inquiry" className="btn">문의사항</Link>
+
+            </div>
         </>
-    
+
     )
 }
 
